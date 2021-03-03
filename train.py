2706e40c1967f2a380e2cf80a097bf88ebc4b920
Sam Freitas <samfreitas@email.arizona.edu>
2021-03-02 17:54:33 -0700

    update train

--- train.py
+++ train.py
@@ -4,12 +4,22 @@ from detecto import core, utils, visualize
 print(torch.cuda.is_available())
 
 dataset = core.Dataset('images_and_annotations')
+
+val_dataset = core.Dataset('validation')
+
+loader = core.DataLoader(dataset, batch_size=1, shuffle=True)
+
 model = core.Model(['WM_well'])
 
 print(dataset._csv.T)
 
-model.fit(dataset)
+losses = model.fit(loader, val_dataset, epochs=10, learning_rate=0.001, 
+                   lr_step_size=5, verbose=True)
+                   
 
 model.save('model_weights.pth')
 
 model = core.Model.load('model_weights.pth', ['WM_well'])
+
+plt.plot(losses)
+plt.show()
